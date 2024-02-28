9af3f96a1cd0d291e51f41eef717f6c4d4edcfc1
JiajunZhang <acediazhang@gmail.com>
2024-02-28 02:06:49 -0600

    leetcode javascript

---
+++ leetcode javascript/2621睡眠函数.js
@@ -0,0 +1,45 @@
+/*
+请你编写一个异步函数，它接收一个正整数参数 millis ，并休眠 millis 毫秒。要求此函数可以解析任何值。
+
+ 
+
+示例 1：
+
+输入：millis = 100
+输出：100
+解释：
+在 100ms 后此异步函数执行完时返回一个 Promise 对象
+let t = Date.now();
+sleep(100).then(() => {
+  console.log(Date.now() - t); // 100
+});
+示例 2：
+
+输入：millis = 200
+输出：200
+解释：在 200ms 后函数执行完时返回一个 Promise 对象
+ 
+
+提示：
+
+1 <= millis <= 1000
+*/
+
+
+
+
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+    return new Promise(res => setTimeout(res, millis));
+}
+
+/** 
+ let t = Date.now()
+ sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
+
+ let t = Date.now()
+ sleep(200).then(() => console.log(Date.now() - t)) // 200
